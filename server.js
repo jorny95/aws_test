f2016c98f8faa88ebe1a284e60bbc9e9c4e4b4dc
jihyun <kjwise1128@gmail.com>
2021-07-22 23:18:33 +0900

    0722

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
+app.use(bodyParser.urlencoded(
+    {extended:false,}
+))
 
 app.get('/',(req,res)=>{
     res.send('hello aws!')
+})
+
+app.post('/',(req,res)=>{
+    console.log('POSTMAN으로 실행함!')
+    res.json(req.body) 
 })
 
 app.listen(3000,()=>{
